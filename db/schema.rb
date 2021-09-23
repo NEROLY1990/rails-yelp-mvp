16bfa3641ffdad46d3859281489221bbb4434f10
LY QUACH <lyllq.quach@gmail.com>
2021-09-23 19:59:20 +0200

    create schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_23_175647) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.integer "phone_number"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
